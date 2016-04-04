5f3face6b0fdaeb18d58b90483fa83a086e2468f
Lewis <lewis500@gmail.com>
2016-04-03 18:39:50 -0700

    cut out a lot of text. has a four rules section now.

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -47,21 +47,21 @@ const AppComponent = React.createClass({
 							The answer is: the Federal Reserve. The Fed constantly creates money to buy very safe debt from big banks, and this is most of what explains the economy's swings.</p>
 						<p>When the Fed suddenly creates money faster,  interest rates (at least short-term ones) fall. This boosts spending by making it cheap to borrow or refinance. For example, North Carolina <a href="http://abc11.com/politics/nc-passes-$2-billion-bond-for-infrastructure/1247482/">will soon borrow $2 billion</a> to build infrastructure <a href="http://connect.nc.gov/">because of low interest rates.</a> The extra spending boosts inflation and/or employment. When the Fed doesn't create money fast enough, spending falls. It might do this on purpose to cut inflation <a href="http://www.carnegie-rochester.rochester.edu/Nov04-pdfs/GK.pdf">like in the early 80's,</a> or by accident <a href="http://fee.org/articles/the-great-depression-according-to-milton-friedman/">like in the 30's.</a>
 						</p>
-						<p>This logic sounds simple, but things get  complicated for three reasons:
-						</p>
+				</div>
+				<div className='special'>
+					<h4>Four rules</h4>
+				</div>
+				<div className='content'>
+						<p>To see how the Fed decides what to do, we'll simulate a simple economy. It has four rules:</p>
 						<ol>
-							<li>People learn, so if <Kat string="\pi" /> is  inflation and <Kat string="\pi_e" /> is <em>expected</em> inflation, then over time <Kat string="\pi_e \rightarrow\pi" />. </li>
-							<li>Workers account for expected inflation. If workers expect 3% inflation, then&mdash;to create jobs&mdash;the Fed must boost spending more than if they expect 1%.</li>
-							<li>Borrowers account for expected inflation. If the "nominal" interest rate is <Kat string="i" /> (the number quoted), then the inflation-adjusted "real" interest rate, <Kat string="r=i-\pi_e"/>, is what matters. So nominal rate is lower than it looks, practically speaking, if expected inflation is high.</li>
-						</ol>
-						<p>These facts lead, in a roundabout way, to two special quantities:</p>	
-							<ol>
-								<li>The <em>natural real interest rate</em>, <Kat string="\bar{r}"/>. If the real rate, <Kat string="r"/>, goes below <Kat string="\bar{r}"/>, then spending clims and unemployment falls. When  <Kat string="r"/> exceeds <Kat string="\bar{r}"/>, they do the opposite.</li>
-								<li>The <em>natural unemployment rate</em>, <Kat string="\bar{u}"/>. If unemployment falls below  <Kat string="\bar{u}"/>, inflation accelerates. If it exceeds <Kat string="\bar{u}"/>, then inflation falls.</li>
+							<li>People predict what inflation will be from experience. So if <Kat string="\pi" /> is <em>current</em> inflation and <Kat string="\pi_e" /> is <em>expected</em> inflation, then over time <Kat string="\pi_e \rightarrow\pi" />. In other words, the expectation tends toward what's actually happening. </li>
+							<li>The "real" interest rate, which takes expected inflation into account, is what matters to employment. If the "nominal" interest rate is <Kat string="i" /> (the rate at which you actually borrow), then the real interest rate <Kat string="r"/> is given by <Kat string="r=i-\pi_e"/>. So if <Kat string="\pi_e"/> rises, then <Kat string="i"/> needs to rise to have the same effect, and vice versa.</li>
+							<li>There is a "natural" real interest rate, <Kat string="\bar{r}"/>. If the real rate drops below <Kat string="\bar{r}"/>, then unemployment falls. And when <Kat string="r"/> exceeds <Kat string="\bar{r}"/>, unemployment climbs.</li>
+							<li>There is a "natural" unemployment rate, <Kat string="\bar{u}"/>. If unemployment <Kat string="u"/> drops below  <Kat string="\bar{u}"/>, then inflation rises (because workers bid up wages). If  <Kat string="u"/> exceeds <Kat string="\bar{u}"/>, then inflation falls. In the US, <Kat string="\bar{u}"/> is around 4%. In European countries with strong unions, it's higher.</li>
 							</ol>
 				</div>
 				<div className='special'>
-					<h4>Simulation</h4>
+					<h4>A simulation</h4>
 				</div>
 				<div className="content">
 						<p>This simulation applies the ideas above to a simple economy. With the <span className="blue">blue</span> dot, control <Kat string="i" col={col["light-blue"]["800"]}/> to stabilize <Kat string="\pi" col={col.orange["500"]}/> and <Kat string="u" col={col.indigo["500"]}/>. Watch <Kat string="\bar{r}" col={col.teal["600"]}/> and <Kat string="\bar{u}" col={col.indigo["500"]}/>, because when <Kat string="r<\bar{r}" col={col.teal["600"]}/>, <Kat string="u" col={col.indigo["500"]}/> falls; and when <Kat string="u<\bar{u}" col={col.indigo["500"]}/>, <Kat string="\pi" col={col.orange["500"]}/> rises.</p>
