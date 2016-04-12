2cbec9920eb99be3085961b65174220b8065e61b
Lewis <lewis500@gmail.com>
2016-04-12 00:49:55 -0700

    fixed some text.

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -77,7 +77,7 @@ const AppComponent = React.createClass({
 						<p style={{textAlign: 'center'}}>With the <span className="blue">blue</span> dot, control <KK S="i" col={CC.i}/> to stabilize <KK S="\pi" col={CC.π}/> and <KK S="u" col={CC.u}/>.  <br/>
 						When <KK S="r<\bar{r}" col={CC.r}/>, <KK S="u" col={CC.u}/> falls. 	When <KK S="r>\bar{r}" col={CC.r}/>, <KK S="u" col={CC.u}/> rises.  <br/> 
 						When <KK S="u<\bar{u}" col={CC.u}/>, <KK S="\pi" col={CC.π}/> rises.  When <KK S="u>\bar{u}" col={CC.u}/>, <KK S="\pi" col={CC.π}/> falls. <br/>
-						You fail if <KK S="u<\bar{u}" col={CC.u}/> or <KK S="\pi" col={CC.π}/> gets too high.
+						You fail if <KK S="u" col={CC.u}/> or <KK S="\pi" col={CC.π}/> gets too high.
 						</p>
 						<br/>
 				<div className='flex-container-row'>
@@ -113,26 +113,28 @@ const AppComponent = React.createClass({
 					<h4>Discussion</h4>
 				</div>
 				<div className="content">
-					<p>This is a very simple model. It is unrealistic in many ways: you can get 0% unemployment; you can't <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/18-negative-interest-rates">set negative interest rates</a>; the natural rates of unemployment and interest don't change (to make play easier). Moreover, the Fed has more tools than <KK S="i" col={CC.i}/>: it can <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/24-rate-pegs">set long-term interest rates</a> or do quantitative easing. But what the simulation does accomplish is to show the Fed's  trade-offs.</p>
-					<p>To learn more about macro, subscribe to <a href="https://itunes.apple.com/us/podcast/macro-musings/id1099277290?mt=2" >David Beckworth's new podcast</a> or read <a href="http://www.brookings.edu/blogs/ben-bernanke">Ben Bernanke's</a> and <a href="http://www.themoneyillusion.com/">Scott Sumner's</a> blogs.	To read more about macroeconomics, check out David Romer's <a href="http://www.amazon.com/Advanced-Macroeconomics-McGraw-Hill-Series-Economics/dp/0073511374">Advanced Macroeconomics</a>. Please don't email me about <a href="http://econlog.econlib.org/archives/2006/09/my_latest_paper.html">Austrian economics</a> or other alternative theories.
+					<p>This is a very simple model. It is unrealistic in many ways: you can get 0% unemployment; there's nothing wrong with deflation; you can't <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/18-negative-interest-rates">set negative interest rates</a>; the natural rates of unemployment and interest don't change (to make play easier). Moreover, the Fed has more tools than <KK S="i" col={CC.i}/>: it can <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/24-rate-pegs">set long-term interest rates</a> or do quantitative easing. But what the simulation does accomplish is to show the Fed's  trade-offs.</p>
+					<p>To learn more about macro, subscribe to <a href="https://itunes.apple.com/us/podcast/macro-musings/id1099277290?mt=2" >David Beckworth's new podcast</a> or read <a href="http://www.brookings.edu/blogs/ben-bernanke">Ben Bernanke's</a> and <a href="http://www.themoneyillusion.com/">Scott Sumner's</a> blogs.	For deep cuts, check out David Romer's <a href="http://www.amazon.com/Advanced-Macroeconomics-McGraw-Hill-Series-Economics/dp/0073511374">Advanced Macroeconomics</a>.
 					</p>
 				</div>
-				<div className='special'>
-					<h4>A rant</h4>
-				</div>
-				<div className="content">
-					<p>As a parting shot I couldn't resist, I'll address the idea <a href="http://www.businessinsider.com/ben-bernanke-on-federal-reserve-hurting-savers-2015-10">the Fed hurts "savers" with low interest rates.</a> The complaint has three problems:</p>
-					<ol>
-						<li>
-							The Fed could only make <KK S="r" col={CC.r}/> higher than <KK S="\bar{r}" col={CC.r} /> at the cost of ever-worsening unemployment.
-						</li>
-						<li>The Fed has legal mandates to keep employment high and inflation low.</li>
-						<li>
-							"Saver" is not an identity. Anyone holding money in the bank can instead pay off debt, buy securities, buy real estate or spend.  If many people want to hold money and few safe borrowers want to invest, then returns to holding money will be low.
-						</li>
-					</ol>
-					<p>Savers' real enemies are (i) <a href="http://www.bloombergview.com/articles/2016-04-11/those-tiny-fees-make-your-financial-adviser-rich">fund managers</a> who underperform index funds; and (ii) lawyers and officials who abuse the legal system&mdash;especially patent and <a href="http://www.planningreport.com/2015/12/21/new-ceqa-study-reveals-widespread-abuse-legal-process-non-environmentalists">land-use law</a>&mdash;in ways that block investment. These lead to low <KK S="\bar{r}" col={CC.r} /></p>
-				</div>
+				{
+				// <div className='special'>
+				// 	<h4>A rant</h4>
+				// </div>
+				// <div className="content">
+				// 	<p>As a parting shot I couldn't resist, I'll address the idea <a href="http://www.businessinsider.com/ben-bernanke-on-federal-reserve-hurting-savers-2015-10">the Fed hurts "savers" with low interest rates.</a> The complaint has three problems:</p>
+				// 	<ol>
+				// 		<li>
+				// 			The Fed could only make <KK S="r" col={CC.r}/> higher than <KK S="\bar{r}" col={CC.r} /> at the cost of ever-worsening unemployment.
+				// 		</li>
+				// 		<li>The Fed has legal mandates to keep employment high and inflation low.</li>
+				// 		<li>
+				// 			"Saver" is not an identity. Anyone holding money in the bank can instead pay off debt, buy securities, buy real estate or spend.  If many people want to hold money and few safe borrowers want to invest, then returns to holding money will be low.
+				// 		</li>
+				// 	</ol>
+				// 	<p>Savers' real enemies are (i) <a href="http://www.bloombergview.com/articles/2016-04-11/those-tiny-fees-make-your-financial-adviser-rich">fund managers</a> who underperform index funds; and (ii) lawyers and officials who abuse the legal system&mdash;especially patent and <a href="http://www.planningreport.com/2015/12/21/new-ceqa-study-reveals-widespread-abuse-legal-process-non-environmentalists">land-use law</a>&mdash;in ways that block investment. These lead to low <KK S="\bar{r}" col={CC.r} /></p>
+				// </div>
+				}
 			</div>
 			<p>Made with React, d3.js, Redux, WebPack and Babel. Thanks to my roommates for playing this boring game.</p>
 			</div>
