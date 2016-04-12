f4450676de8c162800b8a14349841ed418b0e69c
Lewis <lewis500@gmail.com>
2016-04-11 20:27:36 -0700

    fixing text

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -105,6 +105,23 @@ const AppComponent = React.createClass({
 						</ul>
 				</div>
 				</div>
+				<div className='special'>
+					<h4>Food for thought</h4>
+				</div>
+				<div className="content">
+					<p>This is a very simple model. It is unrealistic in several ways, like you can get 0% unemployment and you can't <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/18-negative-interest-rates">set negative interest rates</a>. Also, in real life, the Fed has more tools than <KK S="i" col={CC.i}/>: it can <a href="http://www.brookings.edu/blogs/ben-bernanke/posts/2016/03/24-rate-pegs">set long-term interest rates</a> or do quantitative easing. The simulation just shows the Fed's main trade-offs. To learn more about macroeconomics, check out David Romer's <a href="http://www.amazon.com/Advanced-Macroeconomics-McGraw-Hill-Series-Economics/dp/0073511374">Advanced Macroeconomics</a>. Please don't email me about <a href="http://econlog.econlib.org/archives/2006/09/my_latest_paper.html">Austrian economics</a> or other alternative theories, as I am a only a transportation student with a side interest in macro.</p>
+					<p>One misunderstanding I <em>will</em> address is the idea that <a href="http://www.businessinsider.com/ben-bernanke-on-federal-reserve-hurting-savers-2015-10">the Fed hurts "savers" with low interest rates.</a> The complaint has three problems:</p>
+					<ol>
+						<li>
+							The Fed could only make <KK S="r" col={CC.r}/> higher than <KK S="\bar{r}" col={CC.r} /> temporarily without getting ever-worsening unemployment. Try it in the simulation. The Fed cannot control <KK S="\bar{r}" col={CC.r} />.
+						</li>
+						<li>The Fed has legal mandates to keep employment high and inflation low. No law guarantees a return on safe, liquid assets.</li>
+						<li>
+							"Saver" is not an identity. Anyone holding money in the bank can instead pay off debt, buy securities, buy real estate or spend.  Unfortunately, if many people want to hold money and few safe borrowers want to invest, then returns to money will be low &mdash; even with a gold standard or free banking.
+						</li>
+					</ol>
+					<p>Savers' real enemies are (i) fund managers who collect fees but can't beat index funds; and (ii) lawyers and politicians who abuse the legal system&mdash;like intellectual property and <a href="http://www.planningreport.com/2015/12/21/new-ceqa-study-reveals-widespread-abuse-legal-process-non-environmentalists">land-use law</a>&mdash;in ways that scare off investment in new products, infrastructure and homes.</p>
+				</div>
 			</div>
 			</div>
 		);
